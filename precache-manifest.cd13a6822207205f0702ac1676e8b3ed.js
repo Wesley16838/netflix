d16981f7211c97f87a0c0ad171e43085207e443e
WesleyWong <wesley16838@gmail.com>
2020-06-11 00:53:20 +0800

    Updates

---
+++ precache-manifest.cd13a6822207205f0702ac1676e8b3ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0e0471448a85470c4b1d975c1773ef",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "28343b28cc692712479c",
+    "url": "/netflix/static/css/main.8ef81d12.chunk.css"
+  },
+  {
+    "revision": "96f84ac75beae2570755",
+    "url": "/netflix/static/js/2.4804c2fc.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/netflix/static/js/2.4804c2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28343b28cc692712479c",
+    "url": "/netflix/static/js/main.f4b3bb0d.chunk.js"
+  },
+  {
+    "revision": "b2df484e2ad08f66a6e5",
+    "url": "/netflix/static/js/runtime-main.d221a83e.js"
+  },
+  {
+    "revision": "a8f4c5e1a3b26d0264414e4e855a5d86",
+    "url": "/netflix/static/media/bgimage_new.a8f4c5e1.png"
+  }
+]);
